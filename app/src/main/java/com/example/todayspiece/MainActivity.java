90792299537beb1dcff55e5a92ad81c0503f8728
Yeongil-Park <p03479@naver.com>
2024-11-28 04:08:41 +0900

    기본 캘린더

--- app/src/main/java/com/example/todayspiece/MainActivity.java
+++ app/src/main/java/com/example/todayspiece/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.todayspiece;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CalendarView calendarView = findViewById(R.id.calendarView);
+
+        // 날짜 선택 이벤트 처리
+        calendarView.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            String date = year + "/" + (month + 1) + "/" + dayOfMonth;
+            Toast.makeText(MainActivity.this, "Selected date: " + date, Toast.LENGTH_SHORT).show();
+        });
     }
 }
